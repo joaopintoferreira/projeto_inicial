f586f6c7e9355f635497962f20799e522344e89c
joaopintoferreira <joaopintoferreira6@gmail.com>
2024-06-09 00:34:13 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print("jornada de dados")
+
+idade = 26
+nome = "João"
+
+print(idade)
+
+nova_idade = idade + 2
+print(nova_idade)
+
+lista = {1,2,3,4,5}
+#print(lista)
+
+lista_invertida = lista.reverse()
